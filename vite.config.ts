5cc785ebed025ae745f9d4c87a1af4719056d824
JuliaNezhel <yulyazelikova@gmail.com>
2024-07-31 11:09:05 +0300

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
 
 export default defineConfig({
+  base: '/country/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
